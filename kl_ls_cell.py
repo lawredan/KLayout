191337529313e32574f221801c7315207d15519c
lawredan <128258208+lawredan@users.noreply.github.com>
2023-04-01 21:55:24 -0600

    Added rotation capability Centered cell on origin Need to remove sacrificial layer still

--- kl_ls_cell.py
+++ kl_ls_cell.py
@@ -3,19 +3,24 @@ import math
 import klayout
 import pya
 
-tone = input("(D)ark or (C)lear feature: ")
-size = int(input("Size (in nm) of the line: "))
-pitch = int(input("Pitch (in nm): "))
-length = int(input("Length of the line (in nm): "))
+# tone = input("(D)ark or (C)lear feature: ")
+# size = int(input("Size (in nm) of the line: "))
+# pitch = int(input("Pitch (in nm): "))
+# length = int(input("Length of the line (in nm): "))
+tone = "D"
+size = 100
+pitch = 200
+length = 10000
+angle = 22
 
 layout = pya.Layout()
-Unit = layout.create_cell(f"LS_Array_{tone}_{size}_{pitch}")
+Unit = layout.create_cell(f"LS_Array_{tone}_{size}_{pitch}_{angle}")
 
 l_outline = layout.layer(11,0) #overlay layer for the cell
-l_line = layout.layer(1,0) #line layer
+l_line = layout.layer(2,0) #line layer, sacrificial
 
 #Create the overlay shape
-overlay = Unit.shapes(l_outline).insert(pya.Box((-length/2),0,(length/2),length))
+overlay = Unit.shapes(l_outline).insert(pya.Box((-length/2),-length/2,(length/2),length/2))
 
 #Check pitch of the LS array
 pitch_check = math.floor(length/pitch)
@@ -26,14 +31,26 @@ else:
 
 #Create the LS array
 if iso:
-    line_vert = Unit.shapes(l_line).insert(pya.Box(-size/2,0,size/2,length))
+    line_vert = Unit.shapes(l_line).insert(pya.Box(-size/2,-length/2,size/2,length/2))
 else:
-    for num in range(0,math.floor(pitch_check/2),1):
+    for num in range(0,math.floor(pitch_check),1):
         if num == 0:
-            line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),0,(num*pitch+size)-(size/2),length))
+            line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),-length,(num*pitch+size)-(size/2),length))
         else:
-            line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),0,(num*pitch+size)-(size/2),length))
-            line_vert = Unit.shapes(l_line).insert(pya.Box((-num*pitch)-(size/2),0,(-num*pitch+size)-(size/2),length))
+            line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),-length,(num*pitch+size)-(size/2),length))
+            line_vert = Unit.shapes(l_line).insert(pya.Box((-num*pitch)-(size/2),-length,(-num*pitch+size)-(size/2),length))
+
+#Does the angle transformation for the lines for rotations
+t = pya.ICplxTrans(1,angle,0,0,0)
+Unit.shapes(l_line).transform(t)
+
+#Removes polygons extending beyond the overlay layer by only including those within the overlay region
+r1 = pya.Region(Unit.shapes(l_line))
+r2 = pya.Region(Unit.shapes(l_outline))
+r_and = r1 & r2
+l_diff = layout.layer(1,0)
+r_diff = Unit.shapes(l_diff).insert(r_and)
+
 
 #Export GDS
-layout.write(f"LS_test_t{tone}_s{size}_p{pitch}_l{length}.gds")
+layout.write(f"LS_test_t{tone}_s{size}_p{pitch}_l{length}_a{angle}.gds")
