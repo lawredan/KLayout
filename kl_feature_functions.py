5193b669c5d8e6d738ea2556dee8b99ce161f1ea
lawredan <128258208+lawredan@users.noreply.github.com>
2023-04-03 20:27:35 -0600

    Created contact array function

--- kl_feature_functions.py
+++ kl_feature_functions.py
@@ -3,17 +3,7 @@ import math
 import klayout
 import pya
 
-def ls_cell(tone,size,pitch,length,angle):
-    
-    # tone = input("(D)ark or (C)lear feature: ")
-    # size = int(input("Size (in nm) of the line: "))
-    # pitch = int(input("Pitch (in nm): "))
-    # length = int(input("Length of the line (in nm): "))
-    tone = "D"
-    size = 100
-    pitch = 5001
-    length = 10000
-    angle = 0
+def ls_cell(tone="D",size=100,pitch=200,dimension=10000,angle=0,x2y=1):
 
     layout = pya.Layout()
     Unit = layout.create_cell(f"LS_Array_{tone}_{size}_{pitch}_{angle}")
@@ -22,22 +12,30 @@ def ls_cell(tone,size,pitch,length,angle):
     l_line = layout.layer(2,0) #line layer, sacrificial
 
     #Create the overlay shape
-    overlay = Unit.shapes(l_outline).insert(pya.Box((-length/2),-length/2,(length/2),length/2))
+    overlay = Unit.shapes(l_outline).insert(pya.Box((-dimension/2),-dimension/2,(dimension/2),dimension/2))
 
     #Check pitch of the LS array
-    pitch_check = math.floor(length/pitch)
+    pitch_check = math.floor(dimension/pitch)
     iso = pitch_check < 2 #boolean
+
+    #Define line dimensions
+    l_left = -size/2
+    l_bottom = -dimension/2
+    l_right = size/2
+    l_top = dimension/2
+    l_2bottom = -dimension
+    l_2top = dimension
 
     #Create the LS array
     if iso:
-        line_vert = Unit.shapes(l_line).insert(pya.Box(-size/2,-length/2,size/2,length/2))
+        line_vert = Unit.shapes(l_line).insert(pya.Box(l_left, l_bottom, l_right, l_top))
     else:
         for num in range(0,math.floor(pitch_check),1):
             if num == 0:
-                line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),-length,(num*pitch+size)-(size/2),length))
+                line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)+l_left,l_2bottom,(num*pitch)+l_right,l_2top))
             else:
-                line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)-(size/2),-length,(num*pitch+size)-(size/2),length))
-                line_vert = Unit.shapes(l_line).insert(pya.Box((-num*pitch)-(size/2),-length,(-num*pitch+size)-(size/2),length))
+                line_vert = Unit.shapes(l_line).insert(pya.Box((num*pitch)+l_left,l_2bottom,(num*pitch)+l_right,l_2top))
+                line_vert = Unit.shapes(l_line).insert(pya.Box((-num*pitch)+l_left,l_2bottom,(-num*pitch)+l_right,l_2top))
 
     #Does the angle transformation for the lines for rotations
     t = pya.ICplxTrans(1,angle,0,0,0)
@@ -52,6 +50,57 @@ def ls_cell(tone,size,pitch,length,angle):
 
 
     #Export GDS
-    layout.write(f"LS_test_t{tone}_s{size}_p{pitch}_l{length}_a{angle}.gds")
+    layout.write(f"LS_test_t{tone}_s{size}_p{pitch}_l{dimension}_a{angle}.gds")
 
-ls_cell(1,2,3,4,5)
+
+def contact_cell(tone="D",size=100,pitch=200,dimension=10000,angle=22,x2y=2):
+
+    layout = pya.Layout()
+    Unit = layout.create_cell(f"HD_Array_{tone}_{size}_{pitch}_{angle}_{x2y}to1")
+
+    l_outline = layout.layer(11,0) #overlay layer for the cell
+    l_cont = layout.layer(2,0) #contact layer, sacrificial
+
+    #Create the overlay shape
+    overlay = Unit.shapes(l_outline).insert(pya.Box((-dimension/2),-dimension/2,(dimension/2),dimension/2))
+
+    #Define the true pitch, after taking x2y into account
+    true_pitch = (pitch-size) + (size*x2y)
+
+    #Check pitch of the cont array
+    pitch_check = math.floor(dimension/true_pitch)
+    iso = pitch_check < 2 #boolean
+
+    #Set contact dimensions
+    c_left = -size*x2y/2
+    c_bottom = -size/2
+    c_right = size*x2y/2
+    c_top = size/2
+
+    #Create the cont array
+    if iso:
+        cont = Unit.shapes(l_cont).insert(pya.Box(c_left,c_bottom,c_right,c_top))
+    else:
+        for i in range(-pitch_check,pitch_check,1):
+            for j in range(-pitch_check,pitch_check,1):
+                coord_x = i*true_pitch
+                coord_y = j*true_pitch
+                cont = Unit.shapes(l_cont).insert(pya.Box(coord_x+c_left,coord_y+c_bottom,coord_x+c_right,coord_y+c_top))
+
+
+    #Does the angle transformation for the holes/dots for rotations
+    t = pya.ICplxTrans(1,angle,0,0,0)
+    Unit.shapes(l_cont).transform(t)
+
+    #Removes polygons extending beyond the overlay layer by only including those within the overlay region
+    r1 = pya.Region(Unit.shapes(l_cont))
+    r2 = pya.Region(Unit.shapes(l_outline))
+    r_and = r1 & r2
+    l_diff = layout.layer(1,0)
+    r_diff = Unit.shapes(l_diff).insert(r_and)
+
+
+    #Export GDS
+    layout.write(f"Contact_test_t{tone}_s{size}_p{pitch}_l{dimension}_a{angle}_{x2y}to1.gds")
+
+contact_cell()
